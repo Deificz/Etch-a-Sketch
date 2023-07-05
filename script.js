71843b107d2fc04239a5bbda9aadbb23d125a450
Deificz <99456747+Deificz@users.noreply.github.com>
2023-07-05 17:56:30 +0800

    Added new features and fixed the sketching board

--- script.js
+++ script.js
@@ -15,9 +15,17 @@ let toolHeader = document.createElement(`div`);
 let toolName = document.createElement(`h2`);
 let buttonNew = document.createElement(`button`);
 let buttonReset = document.createElement(`button`);
+let buttonBlack = document.createElement(`button`);
+buttonBlack.classList.add(`btnBlack`);
+let buttonRandom = document.createElement(`button`);
+buttonRandom.classList.add(`btnRandom`);
 
 let divCanvass = document.createElement(`div`);
 divCanvass.classList.add(`canvassDiv`);
+
+let sectionFooter = document.createElement(`section`);
+sectionFooter.classList.add(`footerSection`);
+let footerName = document.createElement(`h3`);
 
 let i;
 let div;
@@ -41,57 +49,96 @@ toolHeader.appendChild(toolName);
 
 divTool.appendChild(buttonNew);
 divTool.appendChild(buttonReset);
+divTool.appendChild(buttonBlack);
+divTool.appendChild(buttonRandom);
 toolName.innerHTML = `TOOLS`
 buttonNew.innerHTML = `<i class="fa-solid fa-plus"></i>`;
 buttonReset.innerHTML = `<i class="fa-solid fa-rotate-right"></i>`;
+buttonBlack.innerHTML = `<i class="fa-solid fa-circle"></i>`;
+buttonRandom.innerHTML = `<i class="fa-solid fa-circle"></i>`;
 
+body.appendChild(sectionFooter);
+sectionFooter.appendChild(footerName);
+footerName.innerHTML = `© Aeron Brylle Lazo`;
 
+//Main
+document.addEventListener("DOMContentLoaded", function(){
+    createDivs(256);
+    divs = document.querySelectorAll(`.divPixel`);
+
+    divs.forEach(div => {
+        div.addEventListener(`mouseover`, () =>{
+        div.style.cssText = `background-color: black;`
+        });
+    });
+
+    buttonBlack.addEventListener(`click`, ()=>{
+            divs.forEach(div => {
+            div.addEventListener(`mouseover`, () =>{
+            div.style.cssText = `background-color: black;`
+            });
+        });
+    });
+
+    buttonRandom.addEventListener(`click`, ()=>{
+        divs.forEach(div => {
+            div.addEventListener(`mouseover`, () =>{
+            div.style.cssText = `background-color: ${generateColor()};`
+            });
+        });
+    });
+    
+
+    buttonNew.addEventListener(`click`, () => {
+        let userInput = prompt(`What size do you want to work on ? `);
+        if(userInput > 100 || userInput < 0 || userInput == ``)
+             alert(`You put an invalid number. Please try again :)`);
+        
+        else{
+            divs.forEach(div => {
+                div.remove();
+            });
+
+            createDivs(userInput * userInput);
+            divs = document.querySelectorAll(`.divPixel`);
+            
+            divs.forEach(div => {
+                div.addEventListener(`mouseover`, () =>{
+                div.style.cssText = `background-color: black;`
+                });
+            });
+        
+            buttonBlack.addEventListener(`click`, ()=>{
+                    divs.forEach(div => {
+                    div.addEventListener(`mouseover`, () =>{
+                    div.style.cssText = `background-color: black;`
+                    });
+                });
+            });
+        
+            buttonRandom.addEventListener(`click`, ()=>{
+                divs.forEach(div => {
+                    div.addEventListener(`mouseover`, () =>{
+                    div.style.cssText = `background-color: ${generateColor()};`
+                    });
+                });
+            });
+    }
+    });
+
+});
 //Create the initial grid
 function createDivs(num){
+    divCanvass.style.gridTemplateColumns = `repeat(${Math.sqrt(num)}, 1fr)`;
+    divCanvass.style.gridTemplateRows = `repeat(${Math.sqrt(num)}, 1fr)`;
     for(i = 0; i < num; i++){
         div = document.createElement(`div`);
         div.classList.add(`divPixel`);
         divCanvass.appendChild(div);
+        divCanvass.insertAdjacentElement(`beforeend`,div);
     }
 }
 
-createDivs(256);
-
-divs = document.querySelectorAll(`.divPixel`);
-
-divs.forEach(div => {
-    div.addEventListener(`mouseover`, () =>{
-        div.style.cssText = `background-color: ${generateColor()};`
-    });
-});
-
-divCanvass.style.cssText = ` grid-template-columns: ${auto.repeat(Math.sqrt(256))}`;
-
-
-//Adding a new grid
-buttonNew.addEventListener(`click`, () => {
-    let userInput = prompt(`What size do you want to work on ? `);
-    if(userInput > 100 || userInput < 0)
-        alert(`You put an invalid number. Please try again :)`);
-    else{
-        divs.forEach(div => {
-            div.remove();
-        });
-        createDivs(userInput * userInput);
-                          
-        
-        divs = document.querySelectorAll(`.divPixel`);
-
-        
-        divs.forEach(div => {
-             div.addEventListener(`mouseover`, () =>{
-             div.style.cssText = `background-color: ${generateColor()};`
-          });
-        });
-        divCanvass.style.cssText = ` grid-template-columns: ${auto.repeat(Math.sqrt(userInput*userInput))}`;
-        
-    }
-});
 
 //Resetting the grid
 buttonReset.addEventListener(`click`, () => {
